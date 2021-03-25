d7d779fd29ffe17066c7954e7b2abb137ec7b0bb
Nikita Kruk <kruk.nikita@gmail.com>
2021-03-25 16:58:41 +0100

    define types for mesh primitives

--- Definitions.hpp
+++ Definitions.hpp
@@ -5,8 +5,14 @@
 #ifndef DEFORMABLECELLMODEL_DEFINITIONS_HPP
 #define DEFORMABLECELLMODEL_DEFINITIONS_HPP
 
+#include <array>
+
 const int kDim = 3;
 const int kFaceDim = 3;
 const int kEdgeDim = 2;
+
+typedef std::array<double, kDim> VectorType; // as an element of a vector space
+typedef std::array<int, kFaceDim> FaceType;
+typedef std::array<int, kEdgeDim> EdgeType;
 
 #endif //DEFORMABLECELLMODEL_DEFINITIONS_HPP
